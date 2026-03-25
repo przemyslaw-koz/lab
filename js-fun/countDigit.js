ccb53393b15c3dde9a86ba521f3e0278e16e9763
przemyslaw-koz <przemyslaw.koz@gmail.com>
2026-03-25 12:50:23 +0100

    added another solution

--- js-fun/countDigit.js
+++ js-fun/countDigit.js
@@ -8,19 +8,25 @@
  * @returns {number} - Number of digits found
  */
 function countDigits(text) {
-  let count=0;
+  let count = 0;
 
-  for (let char of text){
-    if(char >= '0' && char <= '9'){
+  for (let char of text) {
+    if (char >= "0" && char <= "9") {
       count++;
     }
   }
   return count;
 }
 
+const countDigitsSplit = (text) => {
+  return text.split("").filter((char) => char >= "0" && char <= "9").length;
+};
+
 // Example usage
 const input = "abc123xyz45";
 const result = countDigits(input);
+const res2 = countDigitsSplit(input);
 
 console.log(`Input: ${input}`);
 console.log(`Number of digits: ${result}`);
+console.log(`Number of digits: ${res2}`);
