a6f2d100398922c87603fcc47c8df457e957e13c
przemyslaw-koz <przemyslaw.koz@gmail.com>
2026-03-26 11:46:04 +0100

    added di

--- nestjs/messages/src/messages/messages.controller.ts
+++ nestjs/messages/src/messages/messages.controller.ts
@@ -1,14 +1,17 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
-  messagesService: MessagesService;
-
-  constructor() {
-    this.messagesService = new MessagesService();
-  }
+  constructor(public messagesService: MessagesService) {}
 
   @Get()
   listMessages() {
@@ -21,8 +24,12 @@ export class MessagesController {
   }
 
   @Get('/:id')
-  getMessage(@Param() id: string) {
-    console.log(id);
-    return this.messagesService.findOne(id);
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
+    return message;
   }
 }
