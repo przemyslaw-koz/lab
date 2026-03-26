b7a120564469bb79ff92ba85f87f50e0ca85cbfc
przemyslaw-koz <przemyslaw.koz@gmail.com>
2026-03-26 10:50:48 +0100

    messages project

---
+++ nestjs/messages/src/messages/messages.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
+
+@Controller('messages')
+export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    this.messagesService = new MessagesService();
+  }
+
+  @Get()
+  listMessages() {
+    return this.messagesService.findAll();
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content);
+  }
+
+  @Get('/:id')
+  getMessage(@Param() id: string) {
+    console.log(id);
+    return this.messagesService.findOne(id);
+  }
+}
