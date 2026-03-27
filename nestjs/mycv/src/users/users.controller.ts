a495e2784101107348919b404917882ca0d2d058
przemyslaw-koz <przemyslaw.koz@gmail.com>
2026-03-27 14:35:26 +0100

    sigin added

--- nestjs/mycv/src/users/users.controller.ts
+++ nestjs/mycv/src/users/users.controller.ts
@@ -11,20 +11,32 @@ import {
   Query,
   UseInterceptors,
 } from '@nestjs/common';
+import { Serialize } from 'src/interceptors/serializae.interceptor';
+import { AuthService } from './auth.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    this.usersService.create(body.email, body.password);
+    return this.authService.signup(body.email, body.password);
+  }
+
+  @Post('/signin')
+  signin(@Body() body: CreateUserDto) {
+    return this.authService.singin(body.email, body.password);
   }
 
   @Get('/:id')
-  @UseInterceptors(ClassSerializerInterceptor)
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(+id);
 
