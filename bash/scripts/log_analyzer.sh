95f874942a73ef9075897944349985e2f912a746
przemyslaw-koz <przemyslaw.koz@gmail.com>
2026-04-11 21:06:22 +0200

    adding other stuff to script

--- bash/scripts/log_analyzer.sh
+++ bash/scripts/log_analyzer.sh
@@ -3,9 +3,15 @@
 analyze_logs () {
   echo "argument 1: $1"
 
-  validate $1
-  file_exists $1
-  analyze $1
+  validate "$1"
+  file_exists "$1"
+  analyze "$1"
+  check_tr "$1"
+  last_errors "$1"
+
+  if [ -n "$2"]; then
+    filter_errors "$1"
+  fi
 }
 
 validate () {
@@ -27,9 +33,33 @@ file_exists () {
 }
 
 analyze () {
-  echo "number of lines in file: $(wc -l $1)"
-  echo "number of lines with WARNING: $(grep -c "WARNING" $1)"
-  echo "number of lines with ERROR: $(grep -c "ERROR" $1)"
+  echo "number of lines in file: $(wc -l "$1")"
+  echo "number of lines with WARNING: $(grep -c "WARNING" "$1")"
+  echo "number of lines with ERROR: $(grep -c "ERROR" "$1")"
+}
+
+check_tr () {
+  result=$(tr -cs '[:alpha:]' '\n' < "$1" | sort | uniq -c | sort -nr | head -5 )
+  echo "top words:"
+  echo "$result"
+}
+
+last_errors () {
+  result=$(grep "ERROR" < "$1" | tail -5)
+  echo "last errors:"
+  echo "$result"
+}
+
+filter_errors () {
+  local RED=$(tput setaf 1)
+  local YELLOW=$(tput setaf 3)
+  local BOLD=$(tput bold)
+  local RESET=$(tput sgr0)
+
+  local result=$(grep "ERROR" "$1")
+
+  echo "${YELLOW}${BOLD}all ${RED}ERRORS${YELLOW} in logs:${RESET}"
+  echo "${RED}$result${RESET}"
 }
 
 analyze_logs "$1"
