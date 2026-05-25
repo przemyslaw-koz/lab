687122d7184aefd79238d14634ce163521f39372
przemyslaw-koz <przemyslaw.koz@gmail.com>
2026-05-25 17:21:20 +0200

    added histogram

---
+++ python/sample-app-for-prom/histogram_metric.py
@@ -0,0 +1,27 @@
+import http.server
+import os
+from prometheus_client import start_http_server, Histogram
+
+
+REQUEST_LATENCY_TIME = Histogram('request_latency_time', 'Time spent processing request')
+
+
+HOST = os.getenv("HOST", "0.0.0.0")
+PORT = int(os.getenv("PORT", "5000"))
+
+
+class HandleRequests(http.server.BaseHTTPRequestHandler):
+
+    @REQUEST_LATENCY_TIME.time()
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>First python Application</title></head><body style='background-color:blue;'><h1>Hello World!</h1></body></html>", "utf-8"))
+
+
+
+if __name__ == "__main__":
+    start_http_server(5001)
+    server = http.server.HTTPServer((HOST, PORT), HandleRequests)
+    server.serve_forever()
