7b01979191bf2258c182cafcdc8bc752971c9cf5
przemyslaw-koz <przemyslaw.koz@gmail.com>
2026-05-25 15:46:28 +0200

    fixed incorrect property name

--- python/sample-app-for-prom/counter_metric.py
+++ python/sample-app-for-prom/counter_metric.py
@@ -3,7 +3,7 @@ import os
 from prometheus_client import start_http_server, Counter
 
 
-REQUEST_COUNTER = Counter('app_requests_count', 'Total number of HTTP requests', ['python_custom_app', 'endpoint'])
+REQUEST_COUNTER = Counter('app_requests_count', 'Total number of HTTP requests', ['method', 'endpoint'])
 
 
 HOST = os.getenv("HOST", "0.0.0.0")
@@ -14,7 +14,7 @@ class HandleRequests(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
         #REQUEST_COUNTER.inc()
-        REQUEST_COUNTER.labels('get_funcation', self.endpoint).inc()
+        REQUEST_COUNTER.labels(self.command, self.path).inc()
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
