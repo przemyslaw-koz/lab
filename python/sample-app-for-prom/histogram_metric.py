2146b36423ec356caec64968338b8a4cd8c0d5df
przemyslaw-koz <przemyslaw.koz@gmail.com>
2026-05-25 17:44:45 +0200

    added custom buckets

--- python/sample-app-for-prom/histogram_metric.py
+++ python/sample-app-for-prom/histogram_metric.py
@@ -4,7 +4,7 @@ import time
 from prometheus_client import start_http_server, Histogram
 
 
-REQUEST_LATENCY_TIME = Histogram('request_latency_time', 'Time spent processing request')
+REQUEST_LATENCY_TIME = Histogram('request_latency_time', 'Time spent processing request', buckets=(0.001, 0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0))
 
 
 HOST = os.getenv("HOST", "0.0.0.0")
@@ -16,7 +16,6 @@ class HandleRequests(http.server.BaseHTTPRequestHandler):
     @REQUEST_LATENCY_TIME.time()
     def do_GET(self):
         self.send_response(200)
-        time.sleep(1)
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(bytes("<html><head><title>First python Application</title></head><body style='background-color:blue;'><h1>Hello World!</h1></body></html>", "utf-8"))
